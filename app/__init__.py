cc4d5bd0ff95cd575327abdf449484b99af435fb
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2023-08-30 13:56:32 +0530

    updated instance

--- app/__init__.py
+++ app/__init__.py
@@ -14,14 +14,18 @@ DB_NAME="database.db"
 def create_app():
     app= Flask(__name__)
     app.config['SECRET_KEY'] = '#$&^&^WYYDUHS&YWE'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(DB_NAME)
+    #
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(DB_NAME)
+    db_path = os.path.abspath(os.path.dirname(__file__))  # Get the directory of the main script
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(db_path, DB_NAME)}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(db_path, DB_NAME))
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))
     UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/img/profile')
     Excel_FOLDER = os.path.join(APP_ROOT, 'static/excel')
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['Excel_FOLDER'] = Excel_FOLDER
-    
     
     db.init_app(app)
     
@@ -44,14 +48,9 @@ def create_app():
         return Login_admin.query.get(int(id))
     return app
 
-
-
-
-
 def create_database(app):
     if not path.exists('app/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-        
-        
-
+    else:
+        print('Database already exists.')
